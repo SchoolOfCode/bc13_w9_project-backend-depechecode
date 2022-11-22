a0ec482c253d86b6f1f325813184a5224c801ac6
Mrbusy <nickhughes13@googlemail.com>
2022-11-22 15:15:08 +0000

    routes created

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const main = express()
-const port = process.env.PORT;
+const main = express();
+const PORT = 3000;
+const router = require("./Routes/routes")
 
+main.use(express.static("public"));
+main.use(express.json());
+main.use("/api/subjects", router)
 
-main.listen(port, function() {
-  console.log(`server is listening on port ${port}`);
-})
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
